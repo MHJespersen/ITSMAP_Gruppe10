26e07108cc2439c3bf13efee95087474739acdd3
Marie-Louise <maloutroelsen@gmail.com>
2020-11-20 13:41:41 +0100

    DetailsActivity layout

--- app/src/main/java/mhj/Grp10_AppProject/Activities/DetailsActivity.java
+++ app/src/main/java/mhj/Grp10_AppProject/Activities/DetailsActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import mhj.Grp10_AppProject.R;
 import mhj.Grp10_AppProject.ViewModels.DetailsViewModel;
@@ -11,6 +15,9 @@ import mhj.Grp10_AppProject.ViewModels.DetailsViewModelFactory;
 
 public class DetailsActivity extends AppCompatActivity {
     private DetailsViewModel viewModel;
+    private TextView textItemPrice, textItemDesc;
+    private ImageView imgItem;
+    private Button btnBack, btnMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,13 @@ public class DetailsActivity extends AppCompatActivity {
         // https://stackoverflow.com/questions/46283981/android-viewmodel-additional-arguments
         viewModel = new ViewModelProvider(this, new DetailsViewModelFactory(this.getApplicationContext()))
                 .get(DetailsViewModel.class);
+
+        textItemPrice = findViewById(R.id.detailsTextPrice);
+        textItemDesc = findViewById(R.id.detailsTextDesc);
+        imgItem= findViewById(R.id.detailsImage);
+        btnBack = findViewById(R.id.detailsBtnBack);
+        btnMessage = findViewById(R.id.detailsBtnMessage);
+
+        textItemDesc.setMovementMethod(new ScrollingMovementMethod());
     }
 }
