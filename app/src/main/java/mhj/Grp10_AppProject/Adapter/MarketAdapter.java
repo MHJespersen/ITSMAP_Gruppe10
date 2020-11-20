fcacb763e8194b473a089303f7125d0b0e27b638
Mikkel Mahler <mmmikkel@hotmail.com>
2020-11-20 14:47:56 +0100

    Auto stash before merge of "main" and "origin/main"
    "Added default img, glide permission and some changes in acitivityes and layouts for market"

--- app/src/main/java/mhj/Grp10_AppProject/Adapter/MarketAdapter.java
+++ app/src/main/java/mhj/Grp10_AppProject/Adapter/MarketAdapter.java
@@ -3,10 +3,17 @@ package mhj.Grp10_AppProject.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import mhj.Grp10_AppProject.Model.SalesItem;
 import mhj.Grp10_AppProject.R;
 
 
@@ -18,9 +25,16 @@ public class MarketAdapter extends RecyclerView.Adapter<MarketAdapter.ItemViewHo
     }
     private IItemClickedListener listener;
 
+    private List<SalesItem> itemList;
+
     public MarketAdapter(IItemClickedListener listener)
     {
         this.listener = listener;
+    }
+
+    public void updateSalesItemList(List<SalesItem> lists){
+        itemList = lists;
+        notifyDataSetChanged();
     }
 
     @NonNull
@@ -32,9 +46,15 @@ public class MarketAdapter extends RecyclerView.Adapter<MarketAdapter.ItemViewHo
         return vh;
     }
 
+    //Default image inspired by:
+    //https://stackoverflow.com/questions/33194477/display-default-image-in-imageview-if-no-image-returned-from-server
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position)
     {
+        holder.name.setText(itemList.get(position).ItemId);
+        holder.description.setText(itemList.get(position).Description);
+        holder.price.setText((int) itemList.get(position).Price);
+        Glide.with(holder.img.getContext()).load(itemList.get(position).getImage()).placeholder(R.drawable.emptycart).into(holder.img);
     }
 
     @Override
@@ -46,15 +66,33 @@ public class MarketAdapter extends RecyclerView.Adapter<MarketAdapter.ItemViewHo
 
     public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
     {
+        //viewholder ui widget references
+        ImageView img;
+        TextView name;
+        TextView description;
+        TextView price;
+
+        //custom callback interface for user actions
+        IItemClickedListener listener;
+
         public ItemViewHolder(@NonNull View itemView)
         {
             super(itemView);
+
+            //references from layout
+            img = itemView.findViewById(R.id.imgItem);
+            name = itemView.findViewById(R.id.txtItem);
+            description = itemView.findViewById(R.id.txtDescription);
+            price = itemView.findViewById(R.id.txtPrice);
         }
+
+        //react to a click on a listitem
         @Override
         public void onClick(View view)
         {
             listener.OnItemClicked(getAdapterPosition());
         }
+
     }
 }
 
--- app/src/main/java/mhj/Grp10_AppProject/ViewModels/MarketsViewModel.java
+++ app/src/main/java/mhj/Grp10_AppProject/ViewModels/MarketsViewModel.java
@@ -2,11 +2,25 @@ package mhj.Grp10_AppProject.ViewModels;
 
 import android.content.Context;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
+import mhj.Grp10_AppProject.Model.Repository;
+import mhj.Grp10_AppProject.Model.SalesItem;
+
 public class MarketsViewModel extends ViewModel {
+
+    private LiveData<List<SalesItem>> salesitemLiveData;
+    //private final Repository repository;
+
     public MarketsViewModel(Context context)
     {
+        salesitemLiveData = new MutableLiveData<>();
+        //repository = Repository.getInstance(context);
+        //salesitemLiveData = repository.getItems();
     }
 }
 
