4f3fee51ee2a9c174db5c82e31519c2a8e1ba6e6
MikkelMahler <mmmikkel@hotmail.com>
2020-11-20 13:12:45 +0100

    Added model and updated Market Files

--- app/src/main/java/mhj/Grp10_AppProject/Activities/MarketsActivity.java
+++ app/src/main/java/mhj/Grp10_AppProject/Activities/MarketsActivity.java
@@ -1,16 +1,42 @@
 package mhj.Grp10_AppProject.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import mhj.Grp10_AppProject.Adapter.MarketAdapter;
+import mhj.Grp10_AppProject.Database.MarketsDatabase;
 import mhj.Grp10_AppProject.R;
+import mhj.Grp10_AppProject.ViewModels.MarketsViewModel;
+import mhj.Grp10_AppProject.ViewModels.MarketsViewModelFactory;
 
 public class MarketsActivity extends AppCompatActivity {
+
+    MarketsActivity context;
+    MarketsViewModel viewModel;
+
+    //widgets
+    private RecyclerView itemList;
+
+    private MarketAdapter adapter;
+
+    //state
+    private MarketsDatabase db;
+    private SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_markets);
+        context = this;
+
+
+        itemList = findViewById(R.id.rcvItems);
+
+        viewModel = new ViewModelProvider(context, new MarketsViewModelFactory(this.getApplicationContext())).get(MarketsViewModel.class);
     }
 }
