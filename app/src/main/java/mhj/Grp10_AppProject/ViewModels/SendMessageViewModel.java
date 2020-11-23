f79dd1847059a4fe51d6d085a4b93d8304986971
Marcus <cendinho2@hotmail.com>
2020-11-23 23:44:10 +0100

    added setup for messages, in view and VM. Added firebaseDatabase dependency to gradle aswell

--- app/src/main/java/mhj/Grp10_AppProject/ViewModels/SendMessageViewModel.java
+++ app/src/main/java/mhj/Grp10_AppProject/ViewModels/SendMessageViewModel.java
@@ -4,9 +4,16 @@ import android.content.Context;
 
 import androidx.lifecycle.ViewModel;
 
+import mhj.Grp10_AppProject.Model.Repository;
+
 public class SendMessageViewModel extends ViewModel {
+    private final Repository repository;
 
     public SendMessageViewModel(Context context) {
+        repository = Repository.getInstance(context);
+    }
 
+    private void sendMessage(String message) {
+        //repository.sendMessage(message)
     }
 }
