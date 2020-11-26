6d0b69f5113c2120ea736c9bcb6149edde493e5b
Holsko <Mathiasholsko@hotmail.com>
2020-11-26 12:46:22 +0100

    Adding get items to repo

--- app/src/main/java/mhj/Grp10_AppProject/Model/Repository.java
+++ app/src/main/java/mhj/Grp10_AppProject/Model/Repository.java
@@ -3,6 +3,11 @@ package mhj.Grp10_AppProject.Model;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.concurrent.ExecutorService;
 import mhj.Grp10_AppProject.Services.ForegroundService;
 import mhj.Grp10_AppProject.WebAPI.WebAPI;
@@ -10,6 +15,7 @@ import mhj.Grp10_AppProject.WebAPI.WebAPI;
 public class Repository {
 
     private static Repository INSTANCE = null;
+    private FirebaseFirestore firestore;
     private WebAPI api;
 
     private ExecutorService executor;
@@ -27,11 +33,17 @@ public class Repository {
 
     private Repository(Context context)
     {
+        firestore = FirebaseFirestore.getInstance();
     }
 
     public void startMyForegroundService()
     {
         Intent foregroundService = new Intent(con, ForegroundService.class);
         con.startService(foregroundService);
+    }
+
+    public Task<QuerySnapshot> getItems()
+    {
+        return firestore.collection("SalesItems").get();
     }
 }
