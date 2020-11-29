d82561e609b0f8f2a04842a240807f1a2ca3ffd7
Holsko <Mathiasholsko@hotmail.com>
2020-11-29 11:36:35 +0100

    Changing InboxActivity to match Database model, changing fields in InboxViewModel, Removing deleted fields in adapter

--- app/src/main/java/mhj/Grp10_AppProject/Adapter/InboxAdapter.java
+++ app/src/main/java/mhj/Grp10_AppProject/Adapter/InboxAdapter.java
@@ -52,8 +52,7 @@ public class InboxAdapter extends RecyclerView.Adapter<InboxAdapter.InboxViewHol
     @Override
     public void onBindViewHolder(@NonNull InboxViewHolder holder, int position)
     {
-        holder.senderUserName.setText(String.valueOf(messageList.get(position).getSenderId()));
-        holder.itemRegarding.setText(String.valueOf(messageList.get(position).getItemId()));
+        holder.senderUserName.setText(String.valueOf(messageList.get(position).getSender()));
         holder.messageDate.setText(messageList.get(position).getMessageDate());
 
         // Get user -> get user img -> set img
--- app/src/main/java/mhj/Grp10_AppProject/Model/PrivateMessage.java
+++ app/src/main/java/mhj/Grp10_AppProject/Model/PrivateMessage.java
@@ -6,65 +6,36 @@ import java.io.Serializable;
 
 public class PrivateMessage implements Serializable{
 
-    private int messageId, recipientId, senderId, itemId;
-    private String messageBody, messageDate;
+    private String messageBody, messageDate, receiver, sender;
     private Boolean messageRead;
 
     public PrivateMessage(){
     }
 
-    public PrivateMessage(int messageId, int recipientId, int senderId, int itemId, String messageBody, String messageDate, Boolean messageRead) {
-        this.messageId = messageId;
-        this.recipientId = recipientId;
-        this.senderId = senderId;
-        this.itemId = itemId;
+    public PrivateMessage(String receiver, String sender, String messageBody, String messageDate, Boolean messageRead) {
+        this.receiver = receiver;
+        this.sender = sender;
         this.messageBody = messageBody;
         this.messageDate = messageDate;
         this.messageRead = messageRead;
     }
 
     public static PrivateMessage fromSnapshot(DocumentSnapshot d) {
-        PrivateMessage message = new PrivateMessage(Integer.parseInt(d.get("messageId").toString()),
-                Integer.parseInt(d.get("recipientId").toString()),
-                Integer.parseInt(d.get("senderId").toString()),
-                Integer.parseInt(d.get("itemId").toString()),
-                d.get("messageBody").toString(),
-                d.get("messageDate").toString(),
+        PrivateMessage message = new PrivateMessage(d.get("Receiver").toString(),
+                d.get("Sender").toString(),
+                d.get("MessageBody").toString(),
+                d.get("MessageDate").toString(),
                 false);
         return message;
     }
 
-    public int getMessageId() {
-        return messageId;
-    }
+    public String getReceiver(){return receiver;}
 
-    public void setMessageId(int messageId) {
-        this.messageId = messageId;
-    }
+    public void setReceiver(String receiver){this.receiver = receiver;}
 
-    public int getRecipientId() {
-        return recipientId;
-    }
+    public String getSender(){return sender;}
 
-    public void setRecipientId(int recipientId) {
-        this.recipientId = recipientId;
-    }
-
-    public int getSenderId() {
-        return senderId;
-    }
-
-    public void setSenderId(int senderId) {
-        this.senderId = senderId;
-    }
-
-    public int getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(int itemId) {
-        this.itemId = itemId;
-    }
+    public void setSender(){this.sender = sender;}
 
     public String getMessageBody() {
         return messageBody;
